358f8279ded2c567aaf6e8eb25b0948366021b8f
Edwin Ramos <edwinramos269@gmail.com>
2022-05-20 15:26:49 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Shopify front-end challenge
+
+Hey!
+I found and started the application process and this challenge a little late ( on 05/20/2022) and i certainly wanted to submit this as quick as possible while also challenging myself a bit.
+
+You'll find the client side facing code is all in plain DOM js, html and css. I decided it would be fun to not rely on something like react or bootstrap.
+
+Server side code however does use a minimal amount of express to keep my api key secret, if i weren't pressed for time i could probably lifted the server up in straight low level node which would've been interesting.
+
+The application should have the link to the client site, but if not and you find yourself here the link is []().
+
+Hope to hear back soon!
+
+\- Edwin Ramos
---
+++ index.js
@@ -0,0 +1,65 @@
+function createResponseHtml(prompt,response){
+    let htmlString = `
+    <div class="response"><div class="response-prompt">
+        <div class="response-title">
+            <p>Prompt:</p>
+        </div>
+        <div class="human-prompt">
+            <p>${prompt}</p>
+        </div>
+    </div>
+    <br>
+    <div class="response-response">
+        <div class="response-title">
+            <p>Response:</p>
+        </div>
+        <div class="ai-response">
+            <p>${response}</p>
+        </div>
+    </div>
+</div>`
+
+    document.querySelector(".responses-container").insertAdjacentHTML("afterbegin",htmlString)
+};
+
+function pressClear(event){
+    event.preventDefault();
+    const responseList = document.querySelector(".responses-container");
+    while(responseList.firstChild()){
+        responseList.removeChild(responseList.lastChild);
+    }
+}
+
+function pressSubmit(event){
+    event.preventDefault();
+    // get textarea value
+    const promptString = document.querySelector(".textarea").value;
+
+    // check if its empty, if it is return and cancel loader
+    if(!promptString){
+        return;
+    }
+
+    // // start loader
+    // document.querySelector(".responses-header").insertAdjacentElement("beforeend",createLoader());
+        
+    const data = {
+        prompt:promptString
+    };
+    
+    fetch("http://localhost:3000/getResponse", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response)=>response.json())
+    .then((responseData)=>{
+        createResponseHtml(promptString,responseData);
+    });
+};
+
+window.onload = (event) =>{
+    document.querySelector(".submit-button").addEventListener("click", pressSubmit);
+    document.querySelector(".clear-button").addEventListener("click", pressClear);
+};
